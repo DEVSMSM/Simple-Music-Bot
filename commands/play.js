9e91324798eec4c99224844da57a00ef96e7f621
SmSm <86024087+DEVSMSM@users.noreply.github.com>
2023-03-13 15:39:23 +0300

    Create play.js

---
+++ commands/play.js
@@ -0,0 +1,16 @@
+module.exports = {
+  name: 'play',
+  aliases: ['p'],
+  inVoiceChannel: true,
+  run: async (client, message, args) => {
+    const string = args.join(' ')
+    if (!string) return message.channel.send(`${client.emotes.error} | Please enter a song url or query to search.`)
+    client.distube.play(message.member.voice.channel, string, {
+      member: message.member,
+      textChannel: message.channel,
+      message
+    }).catch((err) => {
+      message.reply(`I dont Have Permission To Join Voice Channel`)
+    })
+  }
+}
